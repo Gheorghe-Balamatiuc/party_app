b7d62e85af9e4b10f9fe8733d2ba29cfdcf7084d
Balamatiuc Gheorghe <Balamatiuc Gheorghe@DESKTOP-J23D4K1>
2024-12-08 15:58:50 +0200

    Deleted add party page; Added new api request; Added launch settings with localhost:3000.

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -4,37 +4,16 @@ import 'package:party_app/models/party.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  final String baseUrl = "https://localhost:7041";
+  final String baseUrl = "http://localhost:5028";
 
   Future<List<Party>> fetchParties() async {
-    final response = await http.get(Uri.parse("$baseUrl/api/Party"));
+    final response = await http.get(Uri.parse("$baseUrl/Party"));
 
     if (response.statusCode == 200) {
       Iterable list = json.decode(response.body);
       return List<Party>.from(list.map((model) => Party.fromJson(model)));
     } else {
       throw Exception('Failed to load parties');
-    }
-  }
-
-  Future<Party> createParty(String name, String budget) async {
-    final response = await http.post(
-      Uri.parse('$baseUrl/api/Party'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(<String, String>{
-        'partyName': name,
-        'budget': budget,
-        'memberResponsibilities': [].toString(),
-      }),
-    );
-
-    if (response.statusCode == 201) {
-      return Party.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
-    } else {
-      print(response.body);
-      throw Exception('Failed to create Party.');
     }
   }
 }
--- lib/pages/add_party.dart
+++ lib/pages/add_party.dart
@@ -1,8 +1,6 @@
-import 'dart:async';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:party_app/api/api.dart';
-import 'package:party_app/models/party.dart';
 
 @RoutePage()
 class AddPartyPage extends StatefulWidget {
@@ -15,9 +13,6 @@ class AddPartyPage extends StatefulWidget {
 }
 
 class _AddPartyPageState extends State<AddPartyPage> {
-  final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _budgetController = TextEditingController();
-  Future<Party>? _futureParty;
   final ApiService apiService = ApiService();
 
   @override
@@ -29,53 +24,7 @@ class _AddPartyPageState extends State<AddPartyPage> {
       body: Container(
         alignment: Alignment.center,
         padding: const EdgeInsets.all(8),
-        child: (_futureParty == null) ? buildColumn() : buildFutureBuilder(),
       ),
-    );
-  }
-
-  Column buildColumn() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        TextField(
-          controller: _nameController,
-          decoration: const InputDecoration(hintText: 'Enter Party Name'),
-        ),
-        const SizedBox(height: 16.0),
-        TextField(
-          controller: _budgetController,
-          decoration: const InputDecoration(hintText: 'Enter Budget'),
-          keyboardType: TextInputType.number,
-        ),
-        const SizedBox(height: 16.0),
-        ElevatedButton(
-          onPressed: () {
-            setState(() {
-              _futureParty = apiService.createParty(
-                _nameController.text,
-                _budgetController.text,
-              );
-            });
-          },
-          child: const Text('Create Party'),
-        ),
-      ],
-    );
-  }
-
-  FutureBuilder<Party> buildFutureBuilder() {
-    return FutureBuilder<Party>(
-      future: _futureParty,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return Text('Party Created: ${snapshot.data!.name}');
-        } else if (snapshot.hasError) {
-          return Text('${snapshot.error}');
-        }
-
-        return const CircularProgressIndicator();
-      },
     );
   }
 }
