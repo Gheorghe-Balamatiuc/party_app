464bb3b338dd2d2df864a4feff3b868d8b8c4033
Balamatiuc Gheorghe <Balamatiuc Gheorghe@DESKTOP-J23D4K1>
2024-12-26 22:15:09 +0200

    Forgot to commit party page

--- lib/pages/party.dart
+++ lib/pages/party.dart
@@ -1,21 +1,53 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:party_app/api/api.dart';
+import 'package:party_app/models/party.dart';
 
 @RoutePage()
 class PartyPage extends StatelessWidget {
-  const PartyPage({
+  PartyPage({
     super.key,
     @PathParam('id') required this.partyId,
   });
 
   final int partyId;
+  final apiService = ApiService();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Party $partyId'),
-      ),
+    final party = apiService.fetchParty(partyId);
+
+    return FutureBuilder<PartyWithMembers>(
+      future: party,
+      builder: (BuildContext context, AsyncSnapshot<PartyWithMembers> snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: Text(
+              "Something wrong with message: ${snapshot.error.toString()}",
+              style: const TextStyle(color: Colors.white),
+            ),
+          );
+        } else if (snapshot.connectionState == ConnectionState.done) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(snapshot.data?.name ?? ""),
+            ),
+            body: ListView.builder(
+              itemCount: snapshot.data?.members.length ?? 0,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(snapshot.data?.members[index].firstName ?? ""),
+                  subtitle: Text(snapshot.data?.members[index].lastName ?? ""),
+                );
+              },
+            ),
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
     );
   }
 }
